ecdd14bc4c3c1d2728114e417fff2dc8cad613dc
Denis Borchev <dborchev@gmail.com>
2015-10-19 09:05:16 +0300

    do basic math
    +-*/% and precedence

--- sololearn-cpp/sololearn/main.cpp
+++ sololearn-cpp/sololearn/main.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 int main()
 {
+    //basic vars:
     int myVar = 0;
     int a; int b = 12;
     a = 30;
@@ -11,7 +12,7 @@ int main()
     cout << myVar;
     cout << sum; // outputs 42
 
-
+    //var input-output
     cout << "please enter a number \n";
     int num;
     cin >> num;
@@ -19,7 +20,21 @@ int main()
     cin >> a >> b;
     cout << "Sum is:" << a+b << endl;
 
-
+    //basic arithmetic:
+    int x = 40+60;
+    cout << x <<endl;
+    x = 100 - 60;
+    cout << x;
+    x = 5 * 6;
+    cout << x;
+    x = 10 / 3; //remainder dropped, because int
+    cout << x;
+    x = 25 % 7; //modulus - gives remainder after int div
+    cout << x;
+    x = 5 + 2 * 2; //natural precedence
+    cout << x;
+    x = (5 + 2) * 2; //parentheses
+    cout << x;
 
 
     //probably I won't touch this one right now, if <>0 then IDE prints an error msg
