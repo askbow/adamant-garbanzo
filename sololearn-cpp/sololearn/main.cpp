4d04ddc3dfaba2621187c8e27d81493b7d01af15
Denis Borchev <dborchev@gmail.com>
2015-10-19 08:55:59 +0300

    basic variables tasks
    declaration, output, input, sum

--- sololearn-cpp/sololearn/main.cpp
+++ sololearn-cpp/sololearn/main.cpp
@@ -4,7 +4,24 @@ using namespace std;
 
 int main()
 {
-    cout << "foo\n" << "bar\n\n\n\thhh\t\hhh\n" << endl;
-    cout << "no one" << "expects" << "the" << "spanish" << "inquisition" << endl;
+    int myVar = 0;
+    int a; int b = 12;
+    a = 30;
+    int sum = a + b;
+    cout << myVar;
+    cout << sum; // outputs 42
+
+
+    cout << "please enter a number \n";
+    int num;
+    cin >> num;
+    cout << "give me more numbers!!!!!!! \n";
+    cin >> a >> b;
+    cout << "Sum is:" << a+b << endl;
+
+
+
+
+    //probably I won't touch this one right now, if <>0 then IDE prints an error msg
     return 0;
 }
