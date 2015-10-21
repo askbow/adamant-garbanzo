0f58c4686b0df553f3ef5f64eb8c9018d72b5891
Denis Borchev <dborchev@gmail.com>
2015-10-21 11:45:31 +0300

    do datatypes exercises

--- sololearn-cpp/sololearn/main.cpp
+++ sololearn-cpp/sololearn/main.cpp
@@ -1,109 +1,35 @@
 #include <iostream>
+//already included in iostream:
+//#include <string>
+
+
 
 using namespace std;
 
 int main()
 {
-//if statement
-if (7>4){
-    cout << "spot on";
-}
+// data types
+int answer = 42; int antianswer = -42;
+unsigned long int karamba = 123456789123456789123456789;
 
-//relational ops
-if (7>=4){cout << "kind of";} //gt-eq
-if (7<=4){cout << "strange";} //lt-eq
-if (7==4){cout << "two bits off";} //eq
-if (7!=4){cout << "yep";} //n-eq
+float pi = 3.14;
+double temperature = 123.456;
+long double pressure = -1234.5678;
 
-int a = 3; int b = 42;
-if (b > a) {cout << "the answer is " << b << endl;}
-
-//if-else
-int mark;
-cin >> mark;
-if (mark < 50) {cout << "fail" << endl;}
-else {cout << "congrats =)";}
-
-//if nesting
-if (mark >=50){
-    cout << "good job"<<endl;
-    if (mark >=100){cout << "wow"<<endl;}
-}
-else {cout << "sorry"<<endl;}
+char letter = 'a';
 
 
-//single-statement
-int kk = 10;
-if (kk>=5) cout << "yes\n\n\n\n"<<endl;
-else cout << "no\n\n\n\n"<<endl;
-
-//LOOPs:
-
-//while:
-int num = 1;
-while (num<6){
-    cout << "num: "<<num<<endl;
-    num++;
-}
-
-//merely asking for numbers
-num = 1;
-int number;
-int total = 0;
-while (num<=5){
-    cin >> number;
-    total += number;
-    num++;
-}
-
-cout <<total<<endl;
-
-//for
-for (int x=1; x<10; x++){
-    cout << "i am the model major general\n";
-}
-for (int a = 10; a>=0; a-=3){
-    cout << a<<endl;
-}
-
-//do...while
-//executes at least once...in a while!
-int grok = 0;
-do {
-    cout << a <<endl;
-    a++;
-} while (a<5);
+//needs <string> included:
+string introduction =  "hello. my name is Inigo Montoya. Prepare to die.";
 
 
+bool emcsquare = true;
 
-//switch
-int draks;
-cin >> draks;
-switch(draks){
-case 0:
-    cout << "zero";
-    break;
-case 42:
-    cout << "the right answer";
-    break;
-default:
-    cout << "unknown";
-}
+//name conventions:
+int BackCounterThreshold = 10; //PascalCase
+int backCounter          = 50; //camelCase
 
-
-//logic:
-// && - AND
-// || - OR
-// !  - NOT
-
-cout << "\n\n===>\twe have a nice place here\n\n";
-int age = 23;
-int money = 1000;
-if (age >21 && money >500){
-    cout << "come in ;)\n";
-}
-
-
+int var; int VAR; int Var;     //three different vars - case sensitive
 
 
 
