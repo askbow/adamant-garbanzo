59a9c539f69af69bdac435a65092490059d60eee
Denis Borchev <dborchev@gmail.com>
2015-10-26 10:53:01 +0300

    do class exercises

--- sololearn-cpp/sololearn/main.cpp
+++ sololearn-cpp/sololearn/main.cpp
@@ -10,16 +10,59 @@
 using namespace std;
 
 
+//classes and objects
+class BankAccount{
+ public:
+     //constructor:
+     BankAccount(string nm="", float saldo = 0){
+     balance = saldo;
+     setName(nm);
+     }
+
+     //public methods:
+     string getName(){
+      return name;
+     }
+     void setName(string x){
+     name = x; //in real world, here should be a validation
+     }
+     int deposit(float sum){
+     balance +=sum;
+     return 0;
+     }
+     int withdraw(float sum){
+     if(sum<=balance){ //basic check
+     balance -=sum;
+     return 0;
+     }
+     else{
+        return -1; //not enough money
+     }
+     }//withdraw
+     float readbalance(){
+     return balance;
+     }
+ private:
+    float balance;
+    string name;
+};
 
 
 
 
 int main()
 {
+BankAccount test("kinder", 100); //instantiate
+cout<<test.getName()<<endl;
+test.setName("aloha"); //call public method to set private property
+cout<<test.getName()<<endl;
 
-
-
-
+test.deposit(100.95);
+cout<<test.readbalance()<<endl;
+test.withdraw(1.1);
+cout<<test.readbalance()<<endl;
+test.withdraw(test.readbalance());
+cout<<test.readbalance()<<endl;
     //probably I won't touch this one right now, if <>0 then IDE prints an error msg
     return 0;
 }
